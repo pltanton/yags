6c0aa90c8e0a41120e9bfd378175773fba05eccc
Plotnikov Anton <plotnikovanton@gmail.com>
2017-05-14 18:26:27 +0300

    Remove plugins into separate repositories

--- plugins/maildir/defaults.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/spf13/viper"
-
-func setDefaults(v *viper.Viper) *viper.Viper {
-	v.SetDefault("empty", "{lvl}")
-	v.SetDefault("filled", "{lvl}")
-	return v
-}
--- plugins/timer/time/defaults.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/spf13/viper"
-
-func setDefaults(v *viper.Viper) *viper.Viper {
-	v.SetDefault("timeFormat", "Jan 2 15:04:05")
-	v.SetDefault("pause", 1000)
-	return v
-}
